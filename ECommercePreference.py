6210836da2dc32f45319b60fbe57c81728ac0286
Adarsh Pai <adarsh9pai@gmail.com>
2018-06-10 01:02:33 -0500

    Linear Regression model trained and tested.

--- ECommercePreference.py
+++ ECommercePreference.py
@@ -2,9 +2,11 @@ import pandas
 import seaborn
 import numpy
 import matplotlib.pyplot as pyplot
-
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 DataFrame = pandas.read_csv('ecommerce.csv')
+
 
 print DataFrame.columns
 print DataFrame.head()
@@ -15,3 +17,19 @@ seaborn.jointplot(DataFrame['Time on App'],DataFrame['Yearly Amount Spent'])
 seaborn.pairplot(DataFrame)
 
 pyplot.show()
+
+
+LinearReg = LinearRegression()
+
+X = DataFrame[['Avg. Session Length', 'Time on App',
+       'Time on Website', 'Length of Membership']]
+y = DataFrame['Yearly Amount Spent']
+
+XTrainingSet, XTestingSet, YTrainingSet, YTestingSet = train_test_split( X, y, test_size = 0.35, random_state = 90)
+
+LinearReg.fit(XTrainingSet, YTrainingSet)
+
+LinearRegPredict = LinearReg.predict(XTestingSet)
+
+
+
