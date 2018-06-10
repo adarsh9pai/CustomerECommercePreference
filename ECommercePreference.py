658ac1f96473bfbcdb65712d7818b4b650defd29
Adarsh Pai <adarsh9pai@gmail.com>
2018-06-10 00:42:55 -0500

    Analysis done + Images of plots added.

--- ECommercePreference.py
+++ ECommercePreference.py
@@ -1,4 +1,17 @@
 import pandas
 import seaborn
 import numpy
-import matplotlib
+import matplotlib.pyplot as pyplot
+
+
+DataFrame = pandas.read_csv('ecommerce.csv')
+
+print DataFrame.columns
+print DataFrame.head()
+print DataFrame.describe()
+
+seaborn.jointplot(DataFrame['Time on Website'],DataFrame['Yearly Amount Spent'])
+seaborn.jointplot(DataFrame['Time on App'],DataFrame['Yearly Amount Spent'])
+seaborn.pairplot(DataFrame)
+
+pyplot.show()
