b38f451186bd8445d430bfb07da8d7ce801e0725
Adarsh Pai <adarsh9pai@gmail.com>
2018-06-10 01:39:40 -0500

    Residuals, Conclusion

--- ECommercePreference.py
+++ ECommercePreference.py
@@ -4,6 +4,7 @@ import numpy
 import matplotlib.pyplot as pyplot
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 DataFrame = pandas.read_csv('ecommerce.csv')
 
@@ -12,11 +13,13 @@ print DataFrame.columns
 print DataFrame.head()
 print DataFrame.describe()
 
+
+
 seaborn.jointplot(DataFrame['Time on Website'],DataFrame['Yearly Amount Spent'])
 seaborn.jointplot(DataFrame['Time on App'],DataFrame['Yearly Amount Spent'])
 seaborn.pairplot(DataFrame)
 
-pyplot.show()
+
 
 
 LinearReg = LinearRegression()
@@ -31,5 +34,20 @@ LinearReg.fit(XTrainingSet, YTrainingSet)
 
 LinearRegPredict = LinearReg.predict(XTestingSet)
 
+Coefficients = LinearReg.coef_
+### Intercept = LinearReg.intercept_
+
+pyplot.scatter( YTestingSet, LinearRegPredict)
 
 
+print metrics.mean_absolute_error(YTestingSet, LinearRegPredict)
+print metrics.mean_squared_error(YTestingSet, LinearRegPredict)
+print numpy.sqrt(metrics.mean_squared_error(YTestingSet, LinearRegPredict))
+
+CoefficientsDF = pandas.DataFrame(Coefficients, X.columns, columns = ['Coefficients'])
+
+seaborn.distplot(LinearRegPredict)
+
+print CoefficientsDF
+
+pyplot.show()
